22f2e295a4e53e353ae5de644f70949bc83b16ff
Anubhav Aaryan <74524090+dalaixlmao@users.noreply.github.com>
2024-08-16 13:58:58 +0530

    Update Signin.tsx

--- frontend/src/page/Signin.tsx
+++ frontend/src/page/Signin.tsx
@@ -23,10 +23,8 @@ export default function Signin() {
         password: password,
       });
       setLoading(false);
-      if (response.data.message == "Signed in successfully") {
         localStorage.setItem("token", "Bearer " + response.data.token);
         navigate("/dashboard");
-      }
     } catch (e:any) {
       setError(true);
       if (e.response.data) setErrorM(e.response.data.message);
